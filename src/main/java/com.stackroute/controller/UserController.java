1a82067684808af03f1046b2d09870601399d452
ayush.lohani <ayush.lohani@cgi.com>
2019-10-10 10:41:18 +0530

    Completed SpringMVC Exercise-2

---
+++ src/main/java/com.stackroute/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.stackroute.controller;
+
+import com.stackroute.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class UserController {
+    @RequestMapping(value="/")
+    public String init() {
+        return "index";
+    }
+
+    @RequestMapping(value="/show")
+    public ModelAndView getUserName(HttpServletRequest httpServletRequest){
+        String username=(httpServletRequest.getParameter("username"));
+        String password=(httpServletRequest.getParameter("password"));
+        User user1 = new User();
+        user1.setName(username);
+        user1.setPassword(password);
+        ModelAndView modelAndView = new ModelAndView("display");
+        modelAndView.addObject("user", user1);
+        return modelAndView;
+    }
+}
